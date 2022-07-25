eadd1628575d74f209dc767fed7e7a6ac92e984c
Hanh20021706 <hanhs8155@gmail.com>
2022-07-25 22:32:55 +0700

    html css hompage

--- Client/pages/_app.tsx
+++ Client/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AppPropsWithLayout } from './models/Layout';
+import LayoutClient from '../components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const LayoutWapper = Component.Layout ?? LayoutClient;
+  return (<LayoutWapper> <Component {...pageProps}/></LayoutWapper>)
 }
-
 export default MyApp
