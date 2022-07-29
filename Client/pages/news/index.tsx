61f60508451540f5d72fe5d04f5d154c886cc831
Tuantu <tu0862407432@gmail.com>
2022-07-29 14:46:48 +0700

    create newspage

---
+++ Client/pages/news/index.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import style from "../../styles/news.module.css";
+type Props = {};
+
+const News = (props: Props) => {
+  return (
+    <main>
+      <div className={style.news_page}>
+        <div className={style.news_1}>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+        </div>
+        <div className={style.news_2}>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+          <div>
+          <a href=""><img src="https://i.pinimg.com/564x/dc/2b/50/dc2b504d7e3a729cd230d66cdda750b6.jpg
+" alt="" /></a>     
+            <p>WEDDINGS</p>
+            <h3>
+              An Elegant Backyard Wedding Under a Magnolia Tree in Florence,
+              Alabama
+            </h3>
+          </div>
+        </div>
+       <div className={style.button_newspage}> <button>See more</button></div>
+      </div>
+    </main>
+  );
+};
+
+export default News;
