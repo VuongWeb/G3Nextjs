1d9eb84ad97f7b13135a0009aa059f50597e83cf
TuanTuNguyen <tuntph14942@fpt.edu.vn>
2022-08-02 12:41:53 +0700

    feat : update header

--- Client/components/Header/index.tsx
+++ Client/components/Header/index.tsx
@@ -8,23 +8,23 @@ const Header = (props: Props) => {
         <div className={style.header__page}>
             <div className={style.box__header}>
                 <div className="">
-                    <a href="/"><img className={style.logo__page} src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/New_Balance_logo.svg/2560px-New_Balance_logo.svg.png" alt="" /></a>
+                    <a href=""><img className={style.logo__page} src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/New_Balance_logo.svg/2560px-New_Balance_logo.svg.png" alt="" /></a>
                 </div>
                 <div >
                     <ul className={style.menu__left}>
 
 
-                        <li className=''><a href="/products">Products</a></li>
-                        <li className=''><a href="/about">About</a></li>
-                        <li className=''><a href="/contact">Contact</a></li>
-                        <li className=''><a href="/news">News</a></li>
+                        <li className='/products'><a href="">Products</a></li>
+                        <li className='/about'><a href="">About</a></li>
+                        <li className='/contact'><a href="">Contact</a></li>
+                        <li className='/news'><a href="">News</a></li>
                     </ul>
                 </div>
             </div>
             <div className="">
                 <ul className={style.menu__right}>
                     <li><a href="/login">Login</a></li>
-                    <li><a href="/register">Register</a></li>
+                    <li><a href="/register">icon</a></li>
                 </ul>
             </div>
         </div>
