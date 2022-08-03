783c2a00e39a071f386be535c38e0e7dd643d6f4
long2512 <buidanglong02@gmail.com>
2022-08-04 00:35:01 +0700

    update next Link

--- Client/pages/admin/index.tsx
+++ Client/pages/admin/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import AdminLayout from '../../components/Layout/admin'
 
 type Props = {}
@@ -14,12 +15,13 @@ const AdminPage = (props: Props) => {
           <div className="block sm:flex items-center md:divide-x md:divide-gray-100">
             
             <div className="flex items-center  w-full">
-              <button type="button" data-modal-toggle="add-product-modal" className="text-white bg-red-600 hover:bg-yellow-500 focus:ring-4 focus:ring-cyan-200 font-medium inline-flex items-center rounded-lg text-sm px-3 py-2 text-center sm:ml-auto">
+            <Link href={'/admin/add'}><button type="button" data-modal-toggle="add-product-modal" className="text-white bg-red-600 hover:bg-yellow-500 focus:ring-4 focus:ring-cyan-200 font-medium inline-flex items-center rounded-lg text-sm px-3 py-2 text-center sm:ml-auto">
                 <svg className="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
                 </svg>
                 Add product
-              </button>
+              </button></Link>
+              
             </div>
           </div>
         </div>
@@ -79,14 +81,15 @@ const AdminPage = (props: Props) => {
                       <td className="p-4 whitespace-nowrap text-base font-medium text-gray-900">
                       $149</td>
                     <td className="p-4 whitespace-nowrap space-x-2">
-                      <button type="button" data-modal-toggle="product-modal" className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center">
+                      <Link href={'/id/edit'}><button type="button" data-modal-toggle="product-modal" className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center">
                         <svg className="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                           <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
                           </path>
                           <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
                         </svg>
                         Edit item
-                      </button>
+                      </button></Link>
+                      
                       <button type="button" data-modal-toggle="delete-product-modal" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center">
                         <svg className="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                           <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
